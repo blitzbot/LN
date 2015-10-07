3a16a7a4bae7f5f3f2edac169b3991ba443efbb0
blitzbot <franciscodua@gmail.com>
2015-10-07 08:51:39 +0100

    Tests added (example1.txt). Modified run.sh to run tests.

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 rm -f hours.fst
 rm -f hours.pdf
+rm -f example1.fst result1.pdf
 
 fstcompile --isymbols=data.sym --osymbols=data.sym  transdutor.txt  > hours.fst
 fstdraw  --isymbols=data.sym --osymbols=data.sym  hours.fst | dot -Tpdf > hours.pdf
+
+# Testes
+fstcompile --isymbols=data.sym --osymbols=data.sym  example1.txt  > example1.fst
+
+fstcompose example1.fst hours.fst > result1.fst
+fstdraw --isymbols=data.sym --osymbols=data.sym  result1.fst | dot -Tpdf > result1.pdf
