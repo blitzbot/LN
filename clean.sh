5f2919f03da2303dbc214940f1fc214513ed2f16
blitzbot <franciscodua@gmail.com>
2015-10-20 15:11:07 +0100

    annotated rest of lida corpus, deleted files in root folder

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f *.fst
-rm -f *.pdf
-rm -f testes/*.fst
-rm -f testes/*.pdf
-rm -f testes/pdfs/*.pdf
-rm -f MP1-9.zip
--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sh run.sh
-
-#insert specific tests here
-
-zip -r MP1-9 opcoes.txt comentarios.txt data.sym transdutor.txt transdutor.pdf invertTransdutor.pdf clean.sh run.sh grupo.txt
