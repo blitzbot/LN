ff524156988a6e9b7b29565d1d3b625877702122
blitzbot <franciscodua@gmail.com>
2015-10-24 15:36:34 +0100

    phrase markers added. Only relevant bigrams appear

---
+++ mp2/clean.sh
@@ -0,0 +1,1 @@
+rm *.unigramas *.bigramas
--- mp2/compute_n_grams.py
+++ mp2/compute_n_grams.py
@@ -1,45 +1,5 @@
 from collections import Counter
 
-# def compute_n_grams(n, inputF, parametrizacao, output):
-#     """
-#         recebe tamanho do n-grama (n), o nome do ficheiro com o corpus anotado,
-#         o ficheiro de parametrizacao e a extensao dos ficheiros que irao ser 
-#         escritos. O resultado serao os ficheiros escritos com os n-gramas e a 
-#         contagem para cada um dos lemas em parametrizacao (caso tenham sido 
-#         usados durante a anotacao)
-#     """
-#     f = open(inputF, 'r')
-#     param = open(parametrizacao, 'r')
-
-#     # lemas[0] tem o primeiro lema, lemas[1] o segundo...
-#     lemas = param.readlines()[1].split()
-#     param.close()
-
-#     # cada celula em lines tem uma linha do corpus e respectivo lema
-#     # (OLD) lines = [line.rstrip('\n').lstrip('\t') for line in f]
-#     lines = []
-#     for line in f:
-#         lema = line.split('\t')[0]
-#         # retira o new line do fim e o lema no inicio
-#         sentence = line.rstrip('\n').lstrip('\t')
-#         lines.append([lema, sentence])
-
-#     nGrams = {}
-#     for lema in lemas:
-#         # cada lema tera uma lista de tokens diferente
-#         nGrams[lema] = []
-
-#     for line in lines:
-#         if line[0] in lemas:
-#             # adiciona a lista de ngramas do lema da frase os que encontrou
-#             nGrams[line[0]] += getNgrams(line[1], n)
-    
-#     # para cada lema escrevemos num ficheiro os ngramas
-#     for lema in lemas:
-#         writeNgrams(nGrams, lema, output)
-
-#     f.close()
-
 def compute_n_grams(inputF, parametrizacao):
     """
         recebe tamanho do n-grama (n), o nome do ficheiro com o corpus anotado,
@@ -51,17 +11,19 @@ def compute_n_grams(inputF, parametrizacao):
     f = open(inputF, 'r')
     param = open(parametrizacao, 'r')
 
+    paramLines = param.readlines()
     # lemas[0] tem o primeiro lema, lemas[1] o segundo...
-    lemas = param.readlines()[1].split()
+    lemas = paramLines[1].split()[0:2]
+    # word tem a palavra que se pretende anotar
+    word = paramLines[0].rstrip('\n')
     param.close()
 
     # cada celula em lines tem uma linha do corpus e respectivo lema
-    # (OLD) lines = [line.rstrip('\n').lstrip('\t') for line in f]
     lines = []
     for line in f:
         lema = line.split('\t')[0]
         # retira o new line do fim e o lema no inicio
-        sentence = line.rstrip('\n').lstrip('\t')
+        sentence = '^ ' + line.rstrip('\n').lstrip('\t') + ' $'
         lines.append([lema, sentence])
 
     unigrams = {}
@@ -75,7 +37,7 @@ def compute_n_grams(inputF, parametrizacao):
         if line[0] in lemas:
             # adiciona a lista de ngramas do lema da frase os que encontrou
             unigrams[line[0]] += getNgrams(line[1], 1)
-            bigrams[line[0]] += getNgrams(line[1], 2)
+            bigrams[line[0]] += filterBigrams(getNgrams(line[1], 2), word)
     
     # para cada lema escrevemos num ficheiro os ngramas
     for lema in lemas:
@@ -92,9 +54,9 @@ def compute_n_grams(inputF, parametrizacao):
             writeNgrams(counterUnigrams, lema, '.unigramas')
             writeNgrams(counterBigrams, lema, '.bigramas')
 
+            # alisamento para cada contagem de bigrama
             for key in counterBigrams:
                 counterBigrams[key] = laPlace(counterBigrams[key], n, v)
-                print counterBigrams[key]
             writeNgrams(counterBigrams, lema, 'Alisamento.bigramas')
 
     f.close()
@@ -131,9 +93,15 @@ def getNgrams(line, n):
 
     return nGrams
 
+def filterBigrams(bigrams, word):
+    result = []
+    for bigram in bigrams:
+        if word in bigram.split():
+            result.append(bigram)
+    return result
+
 def laPlace(count, N, vocabulary):
     return (count + 1.0) * N / (N + vocabulary)
 
 if __name__ == '__main__':
     compute_n_grams('foramIrSer-3-fred.out', 'foramParametrizacao.txt')
-    #compute_n_grams(2, 'foramIrSer-3-fred.out', 'foramParametrizacao.txt','.bigramas')
